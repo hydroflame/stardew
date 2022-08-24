dd6aad50d63f631f20049a8a041ffac1d201f3d6
Olivier Gagnon <oli.gagnon4418@gmail.com>
2022-08-24 12:36:14 -0400

    refactor fish page

--- src/App.tsx
+++ src/App.tsx
@@ -13,21 +13,9 @@ import { FishCollectionModal } from "./FishCollectionModal";
 import { Fish, Fishes } from "./Fish";
 import { Division } from "./Division";
 import { CookingCollectionModal } from "./CookingCollectionModal";
-import { Collections, defaultCollections } from "./Collections";
+import { Collections } from "./Collections";
 import { Load, Save } from "./Save";
-
-const splitArray = <T,>(array: T[], split: (t: T) => boolean): [T[], T[]] => {
-  const good = [];
-  const bad = [];
-  for (const t of array) {
-    if (split(t)) {
-      good.push(t);
-    } else {
-      bad.push(t);
-    }
-  }
-  return [good, bad];
-};
+import { FishPage } from "./FishPage";
 
 const App = (): React.ReactElement => {
   const [collections, setCollections] = useState<Collections>(Load());
@@ -39,45 +27,6 @@ const App = (): React.ReactElement => {
     Save(collections);
   }, [collections]);
 
-  const onCaught = (fish: Fish) => {
-    setCollections((old: Collections): Collections => {
-      return {
-        ...old,
-        fishes: { ...old.fishes, [fish.Name]: !old.fishes[fish.Name] },
-      };
-    });
-  };
-  // Find all missing fish first
-  const allMissing: Fish[] = [];
-  for (const [name, caught] of Object.entries(collections.fishes)) {
-    if (caught) continue;
-    const fish = Fishes.find((f) => f.Name === name);
-    if (!fish) continue;
-    allMissing.push(fish);
-  }
-  let others = allMissing.filter((m) => m.Seasons.includes(season));
-  let oneSeason: Fish[];
-  [oneSeason, others] = splitArray(others, (f) => f.Seasons.length === 1);
-  let crabPot: Fish[];
-  [crabPot, others] = splitArray(
-    others,
-    (f) =>
-      f.Locations.length === 1 && String(f.Locations[0]).startsWith("Crab Pot")
-  );
-  const gingerIslandLocations = [
-    Location.GINGER_ISLAND,
-    Location.GINGER_ISLAND_POND,
-    Location.GINGER_ISLAND_RIVER,
-    Location.PIRATE_COVE,
-  ];
-  let gingerIsland: Fish[] = [];
-  [gingerIsland, others] = splitArray(
-    others,
-    (f) =>
-      f.Locations.length === 1 && gingerIslandLocations.includes(f.Locations[0])
-  );
-  let allSeasons: Fish[] = [];
-  [allSeasons, others] = splitArray(others, (f) => f.Seasons.length === 4);
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -115,39 +64,18 @@ const App = (): React.ReactElement => {
           collections={collections}
           setCollection={setCollections}
         />
-        <header className="App-header">
-          {oneSeason.length > 0 && (
-            <Division
-              title={`${season} only`}
-              fishes={oneSeason}
-              onCaught={onCaught}
-            />
-          )}
-          {others.length > 0 && (
-            <Division
-              title={`Multi seasons`}
-              fishes={others}
-              onCaught={onCaught}
-            />
-          )}
-          {allSeasons.length > 0 && (
-            <Division
-              title={`All seasons`}
-              fishes={allSeasons}
-              onCaught={onCaught}
-            />
-          )}
-          {crabPot.length > 0 && (
-            <Division title={`Crab Pot`} fishes={crabPot} onCaught={onCaught} />
-          )}
-          {gingerIsland.length > 0 && (
-            <Division
-              title={`Ginger Island`}
-              fishes={gingerIsland}
-              onCaught={onCaught}
-            />
-          )}
-        </header>
+        <Box
+          sx={{
+            backgroundColor: "#282c34",
+            color: "white",
+          }}
+        >
+          <FishPage
+            season={season}
+            collections={collections}
+            setCollections={setCollections}
+          />
+        </Box>
       </div>
     </Box>
   );
